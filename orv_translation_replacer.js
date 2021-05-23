ccdeaaa98d933c76ce2bbbde8bf6f8caf62ecd06
Yuxianray <narshe.nashley@gmail.com>
2021-05-24 00:32:24 +1000

    Update orv_translation_replacer.js
    Added a reference to which chapter the replacements have been adjusted for.

--- orv_translation_replacer.js
+++ orv_translation_replacer.js
@@ -1,4 +1,6 @@
 /* 
+    -- Updated up to ch.365 --
+
     Description:
     This code was prepared to substitute new translations (ch.363 onward) of the "Omniscient Reader's Viewpoint" web novel on novelchapter.com with older variants, for readers who preferred them.
 
